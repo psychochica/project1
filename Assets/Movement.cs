fd870e2e32965f08378c408da609b0916451b3fa
psychochica <s27216@pjwstk.edu.pl>
2024-10-17 17:24:32 +0200

    animations

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,32 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
+    SpriteRenderer sr;
+    Animator anim;
 
     public float upForce = 100;
     public float speed = 1500;
     public float runSpeed = 2500;
 
     public bool isGrounded = false;
+    bool isLeftShift;
+    float moveHorizontal;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+       
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+       isLeftShift = Input.GetKey(KeyCode.LeftShift);
+       moveHorizontal = Input.GetAxis("Horizontal");
+
+       if (moveHorizontal>0)
+       {
+        sr.flipX = false;
+       }
+       else if (moveHorizontal<0)
+       {
+         sr.flipX = true;
+       }
+
+        if (moveHorizontal == 0)
         {
-            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime, rb.velocity.y,0);
+            anim.SetBool("isRunning",true);
         }
-        else 
+        else
         {
-            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb.velocity.y,0);
+            anim.SetBool(name:"isRunning",true);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -37,6 +56,19 @@ public class Movement : MonoBehaviour
         }
 
     }
+
+    private void FixedUpdate()
+    {
+         if (isLeftShift)
+        {
+            rb.velocity = new Vector3(moveHorizontal * runSpeed * Time.deltaTime, rb.velocity.y,0);
+        }
+        else 
+        {
+            rb.velocity = new Vector3(moveHorizontal * speed * Time.deltaTime, rb.velocity.y,0);
+        }
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
